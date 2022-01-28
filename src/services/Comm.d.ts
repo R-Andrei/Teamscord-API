bb1827bb6905d393607e1862354b29704a338d32
R-Andrei <faragau.rares@gmail.com>
2022-01-28 21:52:03 +0200

    Added ability to start chatrooms/ add people to rooms
    Implemented the ability to start chat rooms - chat rooms start with only the initiating user as participant. Added the ability to add users to chat rooms, by email.

--- src/services/Comm.d.ts
+++ src/services/Comm.d.ts
@@ -1,10 +1,7 @@
-import { Message, Messages, Rooms } from "../types/Message";
+import { Message, Messages, Room, Rooms } from "../types/Message";
 import { Date_meOwO, StwingOwO } from "../types/defaults";
 import {
-    LoginUserProps,
-    RegisterUserProps,
     User,
-    UserWithPassword
 } from "../types/User";
 
 export interface ApiUserProps {
@@ -21,5 +18,7 @@ export interface ApiCommService {
     retrieveRooms(_id: StwingOwO): Promise<Rooms>;
     sendMessage(roomId: StwingOwO, message: StwingOwO, sender: User): Promise<Message>;
     retrieveMessages(roomId: StwingOwO, timestamp: Date | null): Promise<Messages>;
-    addBuddy(roomId: StwingOwO, email: StwingOwO): Promise<User>
+    addBuddy(roomId: StwingOwO, email: StwingOwO): Promise<User>;
+    createRoom(userId: StwingOwO): Promise<Room>;
+    retrieveRoom(roomId: StwingOwO): Promise<Room>;
 }
